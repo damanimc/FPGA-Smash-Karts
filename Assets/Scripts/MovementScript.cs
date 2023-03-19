bc55413d76e3a960abb43fd3a473aa6516899e7c
DamaniMC <84398982+DamaniMC@users.noreply.github.com>
2023-03-19 23:05:38 +0000

    Adding Files

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class MovementScript : MonoBehaviour
+{
+
+    public float movSpeed = 50;
+    public float maxSpeed = 15;
+    public float drag = 0.98f;
+    public float steerAngle = 20;
+    public float traction = 1;
+    private Rigidbody thisRb;
+    private float health = 1.0f;
+    PhotonView view;
+
+    // Variables
+    private Vector3 moveForce;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        thisRb = this.gameObject.GetComponent<Rigidbody>();
+        view = GetComponent<PhotonView>();
+    }
+    void shoot()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (view.IsMine)
+        {
+           
+            // Moving
+            moveForce += transform.forward * movSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+            transform.position += moveForce * Time.deltaTime;
+
+            // Steering
+            float steerInput = Input.GetAxis("Horizontal");
+            transform.Rotate(Vector3.up * steerInput * moveForce.magnitude * steerAngle * Time.deltaTime);
+
+            // drag and max speed limit
+            moveForce *= drag;
+            moveForce = Vector3.ClampMagnitude(moveForce, maxSpeed);
+
+            // traction
+            Debug.DrawRay(transform.position, moveForce.normalized * 3);
+            Debug.DrawRay(transform.position, transform.forward * 3, Color.blue);
+            moveForce = Vector3.Lerp(moveForce.normalized, transform.forward, traction * Time.deltaTime) * moveForce.magnitude;
+            
+        }
+        
+    }
+}
---
+++ Assets/SpawnPlayers.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class SpawnPlayers : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public SmoothFollow camera;
+    public float minX;
+    public float maxX;
+    public float minZ;
+    public float maxZ;
+    public void Start()
+    {
+        Vector3 randomPosition = new Vector3(Random.Range(minX,maxX),0,Random.Range(minZ,maxZ));
+        GameObject player = PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
+        camera.targetObject = player;
+        camera.target = player.transform;
+    }
+}
